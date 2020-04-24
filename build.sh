ee8f21d171993b218f130a81d175a1fe6363bc1c
Fahim G <farg1@student.le.ac.uk>
2020-04-24 17:25:19 +0100

    add run.sh and jenkins.yml

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t fahimg/blueocean-jenkins .
+docker build -t fahimg/pre-installed-plugins .
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run \
+    -p 8080:8080 \
+    fahimg/pre-installed-plugins
